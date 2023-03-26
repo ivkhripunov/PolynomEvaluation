f9ba0a8f1dda85186b566e9fc5bf6a871bdbc472
ivkhripunov <khripunov.iv@phystech.edu>
2023-03-26 23:15:55 +0300

    Refactored, added new tests

--- src/PolynomEvaluation.h
+++ src/PolynomEvaluation.h
@@ -1,7 +1,53 @@
-#ifndef POLYNOMEVALUATION_POLYNOMEVALUATION_H
-#define POLYNOMEVALUATION_POLYNOMEVALUATION_H
+#ifndef POLYNOMEVALUATION_POLYNOM_H
+#define POLYNOMEVALUATION_POLYNOM_H
 
-#include "Polynom.h"
+#include <array>
+#include <cmath>
+
+using indexType = std::size_t;
+using scalar = double;
+
+/*** Функцию для подсчета ***/
+
+namespace Containers {
+    template<typename T, indexType N>
+    using array = std::array<T, N>;
+}
+
+template<typename T, indexType N>
+class Polynom {
+
+private:
+    Containers::array<T, N + 1> data_;
+
+public:
+    constexpr Polynom() {
+        data_.fill(static_cast<T>(0));
+    }
+
+    constexpr Polynom(const Containers::array<T, N + 1> &coeffs) {
+        for (indexType i = 0; i < N + 1; ++i) data_[i] = coeffs[i];
+    }
+
+    const T &operator[](const indexType &i) const {
+        return data_[i];
+    }
+
+    T &operator[](const indexType &i) {
+        return data_[i];
+    }
+
+
+    Polynom<T, N> operator+(const Polynom<T, N> &other) const {
+        Polynom<T, N> result(*this);
+
+        for (auto i = 0; i < N + 1; ++i) {
+            result[i] += other[i];
+        }
+
+        return result;
+    }
+};
 
 template<typename Type>
 struct ReturnStruct {
@@ -11,21 +57,21 @@ struct ReturnStruct {
 
 /**
  * Error-free transformation of the sum of 2 floating point numbers
- * @tparam Type floating point type
+ * @tparam T floating point type
  * @param a floating point number
  * @param b floating point number
  * @return struct: sum of numbers and error
  */
-template<typename Type>
-ReturnStruct<Type> two_sum(const Type &a, const Type &b) {
+template<typename T>
+ReturnStruct<T> two_sum(const T &a, const T &b) {
 
-    ReturnStruct<Type> out;
+    ReturnStruct<T> out;
 
     out.result = a + b;
-    const Type b_virtual = out.result - a;
-    const Type a_virtual = out.result - b_virtual;
-    const Type b_roundoff = b - b_virtual;
-    const Type a_roundoff = a - a_virtual;
+    const T b_virtual = out.result - a;
+    const T a_virtual = out.result - b_virtual;
+    const T b_roundoff = b - b_virtual;
+    const T a_roundoff = a - a_virtual;
     out.error = a_roundoff + b_roundoff;
 
     return out;
@@ -33,15 +79,15 @@ ReturnStruct<Type> two_sum(const Type &a, const Type &b) {
 
 /**
  * Error-free transformation of the product of to floating point numbers with Fused Multiply and add (FMA)
- * @tparam Type floating point type
+ * @tparam T floating point type
  * @param a floating point number
  * @param b floating point number
  * @return struct: result of a * b and error
  */
-template<typename Type>
-ReturnStruct<Type> two_product_fma(const Type &a,
-                                   const Type &b) {
-    ReturnStruct<Type> out;
+template<typename T>
+ReturnStruct<T> two_product_fma(const T &a,
+                                const T &b) {
+    ReturnStruct<T> out;
 
     out.result = a * b;
     out.error = std::fma(a, b, -out.result);
@@ -51,18 +97,18 @@ ReturnStruct<Type> two_product_fma(const Type &a,
 
 /**
  * Horner scheme
- * @tparam Type floating point type
+ * @tparam T floating point type
  * @tparam N polynom degree
  * @param polynom polynom with FP coeffs
  * @param x value for polynom calculation
  * @return polynom value in point x
  */
-template<typename Type, std::size_t N>
-Type horner(const Polynom<Type, N> &polynom, const Type &x) {
+template<typename T, indexType N>
+T horner(const Polynom<T, N> &polynom, const T &x) {
 
-    Type sum = polynom[polynom.get_degree()];
+    T sum = polynom[N];
 
-    for (long long i = polynom.get_degree() - 1; i >= 0; i--) {
+    for (long long i = N - 1; i >= 0; i--) {
         sum = sum * x + polynom[i];
     }
 
@@ -71,18 +117,18 @@ Type horner(const Polynom<Type, N> &polynom, const Type &x) {
 
 /**
  * Compensated Horner Scheme
- * @tparam Type floating point type
+ * @tparam T floating point type
  * @tparam N polynom degree
  * @param polynom polynom with FP coeffs
  * @param x value for polynom calculation
  * @return polynom value in point x
  */
-template<typename Type, std::size_t N>
-Type compensated_horner(const Polynom<Type, N> &polynom, const Type &x) {
+template<typename T, indexType N>
+T compensated_horner(const Polynom<T, N> &polynom, const T &x) {
 
-    Polynom<Type, N - 1> polynom_pi, polynom_sigma;
+    Polynom<T, N - 1> polynom_pi, polynom_sigma;
 
-    ReturnStruct<Type> p, s;
+    ReturnStruct<T> p, s;
     s.result = polynom[N];
 
     for (long long i = N - 1; i >= 0; i--) {
@@ -97,104 +143,4 @@ Type compensated_horner(const Polynom<Type, N> &polynom, const Type &x) {
     return s.result + horner(polynom_pi + polynom_sigma, x);
 }
 
-
-/**
- * Calculating polynom coeffs from given roots
- * @tparam Type floating point type
- * @param roots polynom roots
- * @return vector of coeffs, vector[0] is zero degree
- */
-template<typename Type>
-std::vector<Type> polynomial_coeffs(const std::vector<Type> &roots) {
-
-    std::vector<Type> coeffs(roots.size() + 1, 0);
-    coeffs[0] = static_cast<Type>(1.);
-
-    for (std::size_t i = 0; i < roots.size(); ++i) {
-        for (std::size_t j = roots.size(); j >= 1; j--) {
-            coeffs[j] = coeffs[j - 1] - roots[i] * coeffs[j];
-        }
-
-        coeffs[0] *= -roots[i];
-    }
-
-    return coeffs;
-
-}
-
-/**
- * Calculating condition number
- * @tparam Type floating point type
- * @tparam N polynom degree
- * @param polynom polynom with FP coeffs
- * @param x value for polynom calculation
- * @return condition number
- */
-template<typename Type, std::size_t N>
-Type calc_condition_number(const Polynom<Type, N> &polynom, const Type &x) {
-
-    return compensated_horner(polynom.get_abs(), fabs(x)) / fabs(compensated_horner(polynom, x));
-}
-
-/**
- * Auxilary function for error calculation
- */
-template<typename Type>
-Type calc_gamma(const std::size_t &n, const Type &unit_roundoff) {
-    return n * unit_roundoff / (1 - n * unit_roundoff);
-}
-
-/**
- * Auxiliary function
- */
-template<typename Type>
-Type power(const Type &x, const std::size_t &degree) {
-    Type result = 1;
-    for (std::size_t i = 0; i < degree; ++i) result *= x;
-
-    return result;
-}
-
-/**
- * Basic way of evaluating polynom
- * @tparam Type floating point type
- * @tparam N polynom degree
- * @param polynom polynom with FP coeffs
- * @param x value for polynom calculation
- * @return polynom value in point x
- */
-template<typename Type, std::size_t N>
-Type basic_evaluation(const Polynom<Type, N> &polynom, const Type &x) {
-    Type sum = static_cast<Type>(0);
-
-    for (std::size_t i = 0; i < polynom.get_degree() + 1; ++i) sum += polynom[i] * power(x, i);
-
-    return sum;
-}
-
-template<typename Type, std::size_t N>
-Type calc_error(const Polynom<Type, N> &polynom, const Type &x, const Type &unit_roundoff) {
-
-    Polynom<Type, N - 1> polynom_pi, polynom_sigma;
-
-    ReturnStruct<Type> p, s;
-    s.result = polynom[polynom.get_degree()];
-
-    for (long long i = polynom.get_degree() - 1; i >= 0; i--) {
-
-        p = two_product_fma(s.result, x);
-        s = two_sum(p.result, polynom[i]);
-
-        polynom_pi[i] = p.error;
-        polynom_sigma[i] = s.error;
-    }
-
-    return unit_roundoff * fabs(compensated_horner(polynom, x)) +
-           gamma(4 * polynom.get_degree() + 2) *
-           horner(polynom_pi.get_abs() + polynom_sigma.get_abs(), fabs(x)) +
-           2 * unit_roundoff * unit_roundoff *
-           fabs(compensated_horner(polynom, x));
-}
-
-
-#endif //POLYNOMEVALUATION_POLYNOMEVALUATION_H
+#endif //POLYNOMEVALUATION_POLYNOM_H
